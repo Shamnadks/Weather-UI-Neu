aa7b0fe34a335baa27b85b5c0db829ef15e50877
MOHAMMED RAFHATH <danarafha@gmail.com>
2024-02-09 10:30:57 +0530

    initialised

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "brototype",
+        "appName": "weather",
+        "namespace": "com.brototype.weather",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "brototype",
+        "appName": "weather",
+        "namespace": "com.brototype.weather",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
